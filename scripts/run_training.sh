1ec29ab298e747bd594bbd31ea061bf15e3472ab
wyu-du <duwanyu0706@163.com>
2021-04-06 21:25:57 +0800

    update

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -9,14 +9,14 @@ python ../train_GeDi.py --task_name SST-2 \
   --do_eval  \
   --do_train \
   --logit_scale \
-  --data_dir ../data/AG-news  \
-  --max_seq_length 192 \
+  --data_dir ../data/dailydialog  \
+  --max_seq_length 128 \
   --overwrite_cache \
   --per_gpu_train_batch_size 4 \
   --per_gpu_eval_batch_size  8 \
   --learning_rate $lr  \
   --num_train_epochs 1.0  \
-  --output_dir ../topic_GeDi_retrained \
+  --output_dir ../dailydialog_GeDi_retrained \
   --model_type gpt2  \
   --model_name_or_path gpt2-medium \
   --gen_weight $lambda_ \
