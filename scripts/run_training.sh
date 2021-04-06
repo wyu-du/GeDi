9c62dfbcbc1caf72b0e50dbf98989f7706c3d6cc
wyu-du <duwanyu0706@163.com>
2021-04-06 21:44:08 +0800

    update

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -1,10 +1,10 @@
 
-
+export device_id=2
 export lambda_=0.80
 export lr=2e-5
 
 #final GeDi LM checkpoint saved at --output_dir
-python ../train_GeDi.py --task_name SST-2 \
+CUDA_VISIBLE_DEVICES=$device_id python ../train_GeDi.py --task_name SST-2 \
   --overwrite_output_dir \
   --do_eval  \
   --do_train \
@@ -15,13 +15,13 @@ python ../train_GeDi.py --task_name SST-2 \
   --per_gpu_train_batch_size 4 \
   --per_gpu_eval_batch_size  8 \
   --learning_rate $lr  \
-  --num_train_epochs 1.0  \
+  --num_train_epochs 10  \
   --output_dir ../dailydialog_GeDi_retrained \
   --model_type gpt2  \
   --model_name_or_path gpt2-medium \
   --gen_weight $lambda_ \
   --logging_steps 500 \
-  --save_steps 5000000000 \
+  --save_steps 50000 \
   --code_0 false \
   --code_1 true \
 #  --fp16
