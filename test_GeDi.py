4a5c1338331c5a2b7b0507d356b3042877d608da
wyu-du <duwanyu0706@163.com>
2021-04-07 17:15:56 +0800

    update

--- test_GeDi.py
+++ test_GeDi.py
@@ -1,41 +1,26 @@
 # Adapted from https://github.com/huggingface/transformers/blob/21da895013a95e60df645b7d6b95f4a38f604759/examples/run_glue.py and https://github.com/huggingface/transformers/blob/21da895013a95e60df645b7d6b95f4a38f604759/examples/run_generation.py
 
-
 import argparse
-import glob
 import json
 import logging
 import os
-
-import time
 import numpy as np
 import torch
-
-from tqdm import tqdm, trange
-import sys
-import codecs
 import math
+import os.path
 
 from modeling_gpt2 import GPT2LMHeadModel
-
 from transformers import (
     GPT2Config,
     GPT2Tokenizer
 )
-
-
-import os.path
-import time
 
 try:
     from torch.utils.tensorboard import SummaryWriter
 except ImportError:
     from tensorboardX import SummaryWriter
 
-
 logger = logging.getLogger(__name__)
-
-
 MODEL_CLASSES = {
     "gpt2": (GPT2Config, GPT2LMHeadModel, GPT2Tokenizer),
 }
@@ -47,6 +32,7 @@ def set_seed(args):
         torch.cuda.manual_seed_all(args.seed)
 
 def adjust_length_to_model(length, max_sequence_length):
+    MAX_LENGTH = 128
     if length < 0 and max_sequence_length > 0:
         length = max_sequence_length
     elif 0 < max_sequence_length < length:
@@ -57,9 +43,7 @@ def adjust_length_to_model(length, max_sequence_length):
 
 def main():
     parser = argparse.ArgumentParser()
-
     # Required parameters
-
     parser.add_argument(
         "--model_type",
         default="gpt2",
@@ -78,9 +62,7 @@ def main():
         type=str,
          help="Path to GeDi model. Assumes path from --mode if set to None",
     )
-
     parser.add_argument("--fp16",action="store_true", help="convert GPT-2 XL weights to fp16 (saves signifcant GPU RAM). May sometimes change samples slighty.")
-
     parser.add_argument("--load_in_half_prec",action="store_true", help="loads in half precision to save CPU memory. May sometimes change samples slighty.")
 
     # Other parameters
@@ -99,7 +81,6 @@ def main():
         type=str,
         help="Where do you want to store the pre-trained models downloaded from s3",
     )
-
     parser.add_argument(
         "--do_lower_case", action="store_true", help="Set this flag if you are using an uncased model.",
     )
@@ -120,13 +101,11 @@ def main():
     parser.add_argument("--class_bias", type=float, default=None,
                         help="biases GeDi's classification probabilties"
     )
-
     parser.add_argument("--target_p", type=float, default=0.8,
                         help="In comination with filter_p, saves tokens with above target p probability of being in the correct class "
     )
     parser.add_argument("--do_sample", action="store_true",
                         help="If set to False greedy decoding is used. Otherwise sampling is used. Defaults to True.")
-
     parser.add_argument("--repetition_penalty",
                         default=1.2,
                         type=float,
@@ -139,25 +118,14 @@ def main():
                         help="apply repetition penalty to tokens in coditioning text")
     parser.add_argument("--k", type=float, default=None,
                         help="The number of highest probability vocabulary tokens to keep for top-k-filtering. Between 1 and infinity. Default to 50.",)
-
     parser.add_argument("--p", type=float, default=None,
-                            help="The cumulative probability of parameter highest probability vocabulary tokens to keep for nucleus sampling. Must be between 0 and 1. Default to 1.",
+                        help="The cumulative probability of parameter highest probability vocabulary tokens to keep for nucleus sampling. Must be between 0 and 1. Default to 1.",
                        )
-
-
-
     parser.add_argument("--gen_type", type=str, default="gedi", help="gedi, cclm, or gpt2")
     parser.add_argument("--mode", type=str, default="topic", help="topic, sentiment, detoxify")
-
-
     parser.add_argument("--secondary_code", type=str, default="business", help="secondary topic control code")
-
     parser.add_argument("--gpt3_api_key", type=str, default=None,  help= "GPT-3 api key" )
-
     parser.add_argument("--prompt", type=str, default="",  help= "prompt for generation" )
-
-
-
     args = parser.parse_args()
     assert(args.gen_type=="gedi" or args.gen_type=="cclm" or args.gen_type=="gpt2")
     assert(args.mode=="topic" or args.mode=="sentiment" or args.mode=="detoxify")
@@ -197,33 +165,16 @@ def main():
         if args.class_bias is None:
             args.class_bias = 2.0
 
-
             if args.target_p<1 and args.target_p>0:
                 inv_p = math.log(args.target_p/(1-args.target_p))+args.class_bias
                 args.target_p = 1/(1+math.exp(-1*inv_p))
                 print("changing target p to "  + str(args.target_p) + " to account for class bias term.")
 
-
-
-
-
-
     device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
     args.n_gpu = torch.cuda.device_count()
-
     args.device = device
-
-
-
-
-
     args.model_type = args.model_type.lower()
-
     config_class, model_class, tokenizer_class = MODEL_CLASSES["gpt2"]
-
-
-
-
     tokenizer = tokenizer_class.from_pretrained(args.gen_model_name_or_path,do_lower_case=False)
 
     if args.gen_type == "cclm":
@@ -232,56 +183,38 @@ def main():
         args.length = adjust_length_to_model(args.gen_length,
                              max_sequence_length=model.config.max_position_embeddings)
     else:
-
         if not(args.gpt3_api_key is None):
             print("It's a bit wasteful but our code needs to load GPT-2 even if using GPT-3")
-
-
         if args.load_in_half_prec:
             model = model_class.from_pretrained(args.gen_model_name_or_path,load_in_half_prec=True)
             model.to(args.device)
             #even if using --fp16, apex doesn't accept half prec models, so requires converting to full prec before converting back
             model = model.float()
-
         else:
             model = model_class.from_pretrained(args.gen_model_name_or_path)
             model.to(args.device)
-
-
-
-
         #using fp16 option with GPT2-XL can prevent OOM errors
         if args.fp16:
             try:
                 from apex import amp
             except ImportError:
                 raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
-
-
-
             #opt_level O3 gives fully half precision weights. Okay for GPT-2 as long as we don't need to finetune
             model = amp.initialize(model, opt_level='O3')
             torch.cuda.empty_cache()
-
-
-        args.length = adjust_length_to_model(args.gen_length,
-                             max_sequence_length=1024)
-
+        args.length = adjust_length_to_model(args.gen_length, max_sequence_length=1024)
 
 
     if args.gen_type == "gedi" :
         gedi_model = model_class.from_pretrained(args.gedi_model_name_or_path)
         gedi_model.to(args.device)
-
     else:
         gedi_model=None
 
-    user_prompt = True
-    
+    # Read data
     with open('../data/dailydialog_gen_test.txt', 'r') as f:
         lines = f.read().split('\n')
         lines.remove('')
-        
         
     outs = []
     for i, line in enumerate(lines):
@@ -292,17 +225,16 @@ def main():
         target = line.split('\t')[2]
         tmp = {'context': raw_text, 'target': target, 'intent': da}
         
-        if user_prompt:
-            if args.mode=="topic":
-                code = da
-                bpe_tokens = tokenizer.encode(code)
-                if len(bpe_tokens)>1:
-                    print("Warning! number of bpe tokens for " + code + " is greater than 1, model isn't trained for this, generation is less likely to match topic.")
-                    args.secondary_code = code
-                else:
-                    args.secondary_code = code
-            args.prompt = raw_text
-
+        if args.mode=="topic":
+            code = da
+            bpe_tokens = tokenizer.encode(code)
+            print(bpe_tokens)
+            if len(bpe_tokens)>1:
+                print("Warning! number of bpe tokens for " + code + " is greater than 1, model isn't trained for this, generation is less likely to match topic.")
+                args.secondary_code = code
+            else:
+                args.secondary_code = code
+        args.prompt = raw_text
 
         if args.gen_type=="cclm":
             prefix = tokenizer.encode(args.code_desired)[0]
@@ -317,8 +249,7 @@ def main():
                 text_ids=[prefix]+text_ids
                 start_len = len(tokenizer.decode([prefix]))
         encoded_prompts=torch.LongTensor(text_ids).unsqueeze(0).to(args.device)
-        start_len += len(encoded_prompts.size(1))
-
+        start_len += encoded_prompts.size(1)
 
         if args.gen_type=="gedi" and args.mode=="topic":
             multi_code = tokenizer.encode(args.secondary_code)
@@ -352,7 +283,6 @@ def main():
                                           code_1 = args.code_desired,
                                           multi_code=multi_code
                                           )
-
 
         text = tokenizer.decode(generated_sequence.tolist()[0], clean_up_tokenization_spaces=True)
         text = text[start_len:]
