8d88987f2767a73f345781e664dddd7f3a682171
wyu-du <duwanyu0706@163.com>
2021-04-12 16:47:46 +0800

    update

--- scripts/run_generation.sh
+++ scripts/run_generation.sh
@@ -1,11 +1,11 @@
 
-export device_id=6
+export device_id=2
 
 CUDA_VISIBLE_DEVICES=$device_id python ../test_GeDi.py \
  --gen_length 60 \
  --model_type gpt2 \
  --gen_model_name_or_path microsoft/DialoGPT-medium \
- --gedi_model_name_or_path ../dailydialog_GeDi_retrained/checkpoint-400000 \
+ --gedi_model_name_or_path ../dailydialog_GeDi_DialoGPT/checkpoint-100000 \
  --disc_weight 30 \
  --rep_penalty_scale 10 \
  --filter_p 0.8 \
