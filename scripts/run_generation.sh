475f37ba5ec9cbe5658ab700b3f60eb6fa207a42
wyu-du <duwanyu0706@163.com>
2021-04-15 18:33:59 +0800

    ft base

--- scripts/run_generation.sh
+++ scripts/run_generation.sh
@@ -1,12 +1,12 @@
 
-export device_id=2
+export device_id=3
 
 CUDA_VISIBLE_DEVICES=$device_id python ../test_GeDi.py \
  --gen_length 60 \
  --model_type gpt2 \
- --gen_model_name_or_path microsoft/DialoGPT-medium \
- --gedi_model_name_or_path ../dailydialog_GeDi_DialoGPT/checkpoint-100000 \
- --disc_weight 30 \
+ --gen_model_name_or_path ~/baseline-dialogue/src/microsoft/DialoGPT-medium_dailydialog_raw_base_2021-04-14-09-25-54/checkpoint-8946/ \
+ --gedi_model_name_or_path ../dailydialog_GeDi_DialoGPT/checkpoint-400000 \
+ --disc_weight 200 \
  --rep_penalty_scale 10 \
  --filter_p 0.8 \
  --target_p 0.8 \
